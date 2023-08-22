4e4f8eee2cdb8844efcaad567f6faefb6582459a
Frantass <88505051+MFranco1010@users.noreply.github.com>
2023-08-21 23:54:47 -0300

    gd

--- fmreact/src/components/Card.js
+++ fmreact/src/components/Card.js
@@ -4,8 +4,11 @@ export const Card = () => {
     return (
         <div className='card'>
             <p className='title'> COLOR SWITCH</p>
-            <p className='desc'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quas laborum aliquid dolore, nobis reprehenderit quibusdam autem porro? Adipisci repellendus deleniti aliquid repellat possimus quos voluptatum officia quaerat sint? Dolore! </p>
-            
+            <p className='desc'> My first react proyect, if you press one of the buttons, it will instantly chanche the card color :O</p>
+            <div>
+                
+            </div>
+
         </div>
     )
 }
