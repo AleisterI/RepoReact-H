9a95f9e5b199adcbe77d124264953ac860db1ede
Frantass <88505051+MFranco1010@users.noreply.github.com>
2023-08-21 21:07:06 -0300

    color switch

---
+++ fmreact/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Card = () => {
+    return (
+        <div className='card'>
+            <p className='title'> COLOR SWITCH</p>
+            <p className='desc'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quas laborum aliquid dolore, nobis reprehenderit quibusdam autem porro? Adipisci repellendus deleniti aliquid repellat possimus quos voluptatum officia quaerat sint? Dolore! </p>
+            
+        </div>
+    )
+}
